d6254cf453fe12954f83f20ed1ea825571755e64
sebas1541 <sebastian.ca0102@gmail.com>
2025-02-18 11:12:57 -0500

    Add documentation for Calculator class

--- src/model/Calculator.java
+++ src/model/Calculator.java
@@ -1,7 +1,20 @@
 package model;
 
+/**
+ * La clase Calculator proporciona operaciones aritméticas básicas.
+ * 
+ * Métodos:
+ * - add: Suma dos enteros.
+ * - subtract: Resta el segundo entero del primero.
+ * - multiply: Multiplica dos enteros.
+ * - divide: Divide el primer entero por el segundo.
+ * 
+ * Autor: Gabriel Castillo
+ */
+
 public class Calculator {
 
+  
   public int add(int num1, int num2) {
     return num1 + num2;
   }
